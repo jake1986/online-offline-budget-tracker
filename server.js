6dded24e7b970bdaeaf56313e3eacc20eebca966
Jake Rhoads <jake.rhoads@gmail.com>
2020-06-20 12:29:41 -0400

    commented out line 28 from server.js

--- server.js
+++ server.js
@@ -26,8 +26,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://Online-Offline-Budget-Tra
   useFindAndModify: false
 });
 // // if deployed, use the deployed database. Otherwise, use the local mongoHeadlines
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(MONGODB_URI);
+
 
 // routes
 app.use(require("./routes/api.js"));
